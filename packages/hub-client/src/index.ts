748952a65d2886d6624e542d0577fd5a71c5ef07
Alexander Guy <alexander.guy@pm.me>
2026-05-05 15:55:55 -0700

    Add createInstanceSession factory with comprehensive tests

--- packages/hub-client/src/index.ts
+++ packages/hub-client/src/index.ts
@@ -1,1 +1,21 @@
 export type { InstanceEvent, MailAddress, AgentActivity } from "./types";
+export { type Transport, ApiError, createBrowserTransport } from "./transport";
+export {
+  shouldShowMail,
+  mailToEvent,
+  mailDeliveryToEvent,
+  turnToEvent,
+  parseFromHeader,
+  extractBodyText,
+  formatAddress,
+  isAgentAddress,
+  resolveAgentAddress,
+  resolveAgentRecipient,
+  type MailDeliveryData,
+} from "./transforms";
+export {
+  sessionEndedEvent,
+  MailDeliveredEvent,
+  TurnCommittedEvent,
+} from "./validators";
+export { createInstanceSession, type InstanceSession } from "./session";
