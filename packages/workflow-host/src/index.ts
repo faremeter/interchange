76dfcc2c2172794c48e7a74518a4c672818c7094
Alexander Guy <alexander.guy@pm.me>
2026-06-11 15:45:02 -0600

    Export the production runtime adapters from @intx/workflow-host

--- packages/workflow-host/src/index.ts
+++ packages/workflow-host/src/index.ts
@@ -1,2 +1,19 @@
-// Exports are added by subsequent commits.
-export {};
+export {
+  createWorkflowRunRepoStore,
+  type WorkflowRunRepoStoreOpts,
+} from "./adapters/repo-store";
+export {
+  createWorkflowRunBlobSubstrate,
+  type WorkflowRunBlobSubstrateOpts,
+} from "./adapters/blob-substrate";
+export {
+  createWorkflowStepInvoker,
+  type StepEnvBase,
+  type WorkflowStepInvokerOpts,
+} from "./adapters/step-invoker";
+export {
+  createWorkflowSpawnChild,
+  type ChildTerminalStatus,
+  type RunChildWorkflow,
+  type WorkflowSpawnChildOpts,
+} from "./adapters/spawn-child";
